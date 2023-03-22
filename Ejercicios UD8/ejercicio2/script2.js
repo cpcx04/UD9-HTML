b8a129992e9081751ec38fbfc37730f5c6cfa28e
cpcx04 <pulidocabellochristian@gmail.com>
2023-03-22 14:12:20 +0100

    Yeah

---
+++ Ejercicios UD8/ejercicio2/script2.js
@@ -0,0 +1,10 @@
+/**
+ * Creamos la variable donde irá el mensaje de texto aunque podemos
+ * poner el mensaje directamente en la alerta
+*/
+
+let mensaje;
+//Instanciamos la variable con el mensaje que vamos a mostrar
+mensaje="Hola mundo!!"
+
+alert(mensaje);
---
+++ Ejercicios UD8/ejercicio3/script3.js
@@ -0,0 +1,13 @@
+/**
+ * Creamos la variable donde irá el mensaje de texto aunque podemos
+ * poner el mensaje directamente en la alerta
+*/
+
+let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+let texto = "";
+//Creamos el for con una variable i que nos contará todos los meses del año, es decir
+//recorrerá el array.
+for (let i of meses) {
+    alert(meses[i]);
+    i++
+}
