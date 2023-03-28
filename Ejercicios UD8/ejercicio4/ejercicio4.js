6ac163161746a43cfc8ab0f7671851b4684eedde
Cristian Pulido <pulidocabellochristian@gmail.com>
2023-03-28 11:57:36 +0200

    ljkasd

--- Ejercicios UD8/ejercicio4/ejercicio4.js
+++ Ejercicios UD8/ejercicio4/ejercicio4.js
@@ -1,9 +1,14 @@
-var valores = [true, 5, false, "hola", "adios", 2];
+let valores = [true, 5, false, "hola", "adios", 2];
 
-for (let i of valores) {
-    if(valores[i]>valores[i++]){
-        alert(i)
-    }else
-    alert(i)
-
+if (valores[3] < valores[4]) {
+    alert(valores[3] + " es mas pequeño que " + valores[4])
+} else if (valores[3] > valores[4]) {
+    alert(valores[3] + " es mayor que " + valores[4])
+} else {
+    alert(valores[3] + " es igual que " + valores[4])
 }
+
+if (valores[0] == valores[2]) {
+
+} else
+    alert("El resultado es true")
